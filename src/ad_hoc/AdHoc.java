b30c35358dab47e008cbdfc4754b1a4c6c16a57c
emec delam <kiwi.bp.surviv@gmail.com>
2023-09-16 19:24:57 +0200

    ad_hoc optimization

--- src/ad_hoc/AdHoc.java
+++ src/ad_hoc/AdHoc.java
@@ -1,22 +1,33 @@
 package ad_hoc;
-import java.util.Arrays;
+
 import java.util.Scanner;
 public class AdHoc {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
         int q = Integer.parseInt(scanner.nextLine());
-
         int[] array = generateArray(n);
         for (int i = 2; i < q+2; i++){
-            String[] line = scanner.nextLine().split(" ");
+            String lineString = scanner.nextLine();
+            String[] line = lineString.split(" ");
             int p = Integer.parseInt(line[0]);
             int r = Integer.parseInt(line[1]);
-            int arraylength = (int) Math.pow(2,r-1);
-            System.out.println(firstAndLastValOfArray(divideArrayByTwo(array,arraylength,p)));
+            int arraylength = (int) Math.pow(2,r);
+            int subArrayIndex = p / arraylength;
+            // edge cases for 2 1, 4 1, 4 2, 6 1, 8 1, 8 2 etc basically the right part on every r
+            if (p%2 == 0){
+                float res = p;
+                for (int j = 0; j < r; j++){
+                    res /= 2;
+                }
+                if (Math.floor(res) == res){
+                    subArrayIndex -= 1;
+                }
+            }
+            String res = firstAndLastValOfArray(generateSubSetNotIncluded(generateOverSet(array,arraylength,subArrayIndex),p));
+            System.out.println(res);
         }
         scanner.close();
-
     }
     public static int[] generateArray(int n) {
         int[] numberArray = new int[n];
@@ -28,32 +39,26 @@ public class AdHoc {
         return numberArray;
     }
     public static String firstAndLastValOfArray(int[] array){
-        return String.valueOf(array[0]) + " " + String.valueOf(array[array.length-1]);
+        return (array[0]) + " " +(array[array.length-1]);
     }
     public static boolean isInArraySlice(int[] array, int start, int end, int val){
         return array[start] <= val && array[end] >= val;
     }
-    public static int[] divideArrayByTwo(int[] array,int length,int value) {
-        if (array.length > 1) {
-            int middle = (array.length / 2);
-            int middleIndex = middle -1;
-            if (array.length/2 == length) {
-                int[] res = new int[array.length/2];
-                if (isInArraySlice(array,0,middleIndex,value)){
-                    System.arraycopy(array, middle, res, 0, array.length / 2);
-                } else {
-                    System.arraycopy(array, 0, res, 0, array.length / 2);
-                }
-                return res;
-            } else {
-            int[] newArray = new int[array.length/2];
-            if (isInArraySlice(array,0,middleIndex,value)){
-                System.arraycopy(array, 0, newArray, 0, array.length / 2);
-            } else {
-                System.arraycopy(array, middle, newArray, 0, array.length / 2);
-                }
-                return divideArrayByTwo(newArray,length,value);
-            }
+    public static int[] generateOverSet(int[] array, int length, int index){
+        int[] res = new int[length];
+        System.arraycopy(array,index*length,res,0,length);
+        return res;
+    }
+    public static int[] generateSubSetNotIncluded(int[] array,int value){
+        if (array.length > 1){
+        int[] res = new int[array.length/2];
+        if (isInArraySlice(array,0,array.length/2-1,value)){
+            System.arraycopy(array,array.length/2,res,0,array.length/2);
+
+        } else {
+            System.arraycopy(array,0,res,0,array.length/2);
+        }
+        return res;
         } else {
             return array;
         }
--- src/ad_hoc/AdHocTest.java
+++ src/ad_hoc/AdHocTest.java
@@ -5,12 +5,63 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
-import ad_hoc.AdHoc;
 class AdHocTest {
     @Test
     public void testMain(){
-        String input1 = "8\n3\n3 1\n7 2\n2 3\n";
-        String output1 = "4 4\r\n5 6\r\n5 8\r\n";
+        String input1 = """
+                8
+                24
+                1 1
+                1 2
+                1 3
+                2 1
+                2 2
+                2 3
+                3 1
+                3 2
+                3 3
+                4 1
+                4 2
+                4 3
+                5 1
+                5 2
+                5 3
+                6 1
+                6 2
+                6 3
+                7 1
+                7 2
+                7 3
+                8 1
+                8 2
+                8 3
+                """;
+        String output1 = """
+                2 2\r
+                3 4\r
+                5 8\r
+                1 1\r
+                3 4\r
+                5 8\r
+                4 4\r
+                1 2\r
+                5 8\r
+                3 3\r
+                1 2\r
+                5 8\r
+                6 6\r
+                7 8\r
+                1 4\r
+                5 5\r
+                7 8\r
+                1 4\r
+                8 8\r
+                5 6\r
+                1 4\r
+                7 7\r
+                5 6\r
+                1 4\r
+                """;
         InputStream originalSystemIn = System.in;
         System.setIn(new ByteArrayInputStream(input1.getBytes()));
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -20,8 +71,7 @@ class AdHocTest {
         try {
             AdHoc.main(new String[]{});
             String actualOutput = outputStream.toString();
-            String expectedOutput = output1;
-            assertEquals(expectedOutput, actualOutput);
+            assertEquals(output1, actualOutput);
         } finally {
             System.setIn(originalSystemIn);
             System.setOut(originalSystemOut);
