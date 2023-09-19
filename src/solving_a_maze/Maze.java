61b98b3f5a8dcb12e3f20c9de23861a9997a046c
Kiwi10101001 <119571167+Kiwi10101001@users.noreply.github.com>
2023-09-19 22:35:36 +0200

    maze class final

--- src/solving_a_maze/Maze.java
+++ src/solving_a_maze/Maze.java
@@ -7,13 +7,29 @@ public class Maze {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        System.out.println(m);
+        scanner.nextLine();
         char[][] mazeChar = new char[n][m];
-        for (int i = 0; i <= n; i++){
+        for (int i = 0; i < n; i++){
             String line = scanner.nextLine();
             mazeChar[i] = lineToChar(line);
         }
-        System.out.println(findPath(n,m,mazeChar,charFinder(mazeChar,'S'),charFinder(mazeChar,'T')));
+        ArrayList<int[]> path = findPath(n,m,mazeChar,charFinder(mazeChar,'S'),charFinder(mazeChar,'T'));
+        if (path != null){
+            for (int[] coor : path){
+                System.out.println(coor[0] + " "+ coor[1]);
+            }
+        } else {
+            System.out.println("Nopath");
+        }
+        scanner.close();
+    }
+    public static void printMaze(char[][] maze) {
+        for (char[] chars : maze) {
+            for (char aChar : chars) {
+                System.out.print(aChar);
+            }
+            System.out.println();
+        }
     }
     public static char[] lineToChar(String line){
         char[] res = new char[line.length()];
@@ -22,16 +38,15 @@ public class Maze {
         }
         return res;
     }
-    public static int[] charFinder(char[][] maze,char letter){
-        for (int i = 0; i <= maze.length; i++){
-            for (int j = 0; j <= maze[i].length; i++){
-                if (maze[i][j] == letter){
-                    return new int[]{i,j};
+    public static int[] charFinder(char[][] maze, char letter) {
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[i].length; j++) {
+                if (maze[i][j] == letter) {
+                    return new int[]{i, j};
                 }
             }
         }
         return null;
-
     }
     public static ArrayList<int[]> findPath(int n, int m, char[][] maze, int[] start, int[] end) {
         // initialize the queue and visited matrix
@@ -44,6 +59,9 @@ public class Maze {
         // in practice you might want to stop as soon as end is visited
         while(!Q.isEmpty()) {
             int[] u = Q.poll();
+            if (maze[u[0]][u[1]] == 'T') {
+                break; // Exit the loop
+            }
             // we are now processing node u
             for(int[] d : dir) {
                 int i = u[0] + d[0];
@@ -55,6 +73,14 @@ public class Maze {
                     visited[i][j] = true;
                     // set the parent of (i, j) to be u
                     parent[i][j] = u;
+                }
+                if(!visited[i][j] && maze[i][j] == 'T') {
+                    // node (i, j) has not yet been visited and is not a wall, add it
+                    Q.add(new int[] {i, j});
+                    visited[i][j] = true;
+                    // set the parent of (i, j) to be u
+                    parent[i][j] = u;
+                    break;
                 }
             }
         }
@@ -69,6 +95,7 @@ public class Maze {
             cur = parent[cur[0]][cur[1]];
         }
         // reverse and return the path
+        path.add(start);
         Collections.reverse(path);
         return path;
     }
